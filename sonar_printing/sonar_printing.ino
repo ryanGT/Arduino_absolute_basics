78638af7614ce87d0535fb6e7ff2c6f9ce3abcf0
Ryan Krauss <ryanwkrauss@gmail.com>
2016-11-30 07:28:38 -0500

    added sonar printing example

---
+++ sonar_printing/sonar_printing.ino
@@ -0,0 +1,49 @@
+/*
+Code adapted from 
+http://www.instructables.com/id/Simple-Arduino-and-HC-SR04-Example/
+HC-SR04 Ping distance sensor
+VCC to arduino 5v 
+GND to arduino GND
+Echo to Arduino pin 13 
+Trig to Arduino pin 12
+*/
+
+#define trigPin 13
+#define echoPin 12
+
+float cm_per_microsecond = 0.03432;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  long duration;
+  float distance;
+  digitalWrite(trigPin, LOW);
+  // code from the internet has these fairly specific delays
+  // they seem arbitrary; I have not read the spec on the sonar
+  // sensor to know if they are required or if they are the best values
+  delayMicroseconds(2); 
+  digitalWrite(trigPin, HIGH);
+  //  delayMicroseconds(1000);//<-- internet code said the author had
+  //                                removed this line
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);//<-- this is a pretty cool Arduino function
+  //                                       You may want to bring up it
+  //                                       documentation from the Arduino website
+  Serial.print("duration: ");
+  Serial.println(duration);
+  distance = (duration/2) * cm_per_microsecond;
+  if (distance >= 200 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
